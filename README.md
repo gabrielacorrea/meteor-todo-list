fa8d6997040014f0cc2ce40e5517376e03c92d65
Gabriela Correa <gabriela_correa@gap.com>
2015-09-14 23:25:52 -0300

    tutorial: step 4

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # meteor-todo-list
+
+Create the app: meteor create project-name
+meteor create simple-todos
+
+Run the app: meteor
+
+Structure:
+<head>
+
+<html>
+
+<template>
+Everything inside is compiled into Meteor templates, which can be included inside HTML with {{> templateName}} or referenced in your JavaScript with Template.templateName.
+
--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,28 @@
+Tasks = new Mongo.Collection("tasks");
+ 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var taskToAdd = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: taskToAdd,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
