0a304ac3ae26ebc693cfa0129af1a104ab95f278
Gabriela Correa <gabriela_correa@gap.com>
2015-09-17 23:06:01 -0300

    insere, atualiza e remove tarefas, filtro com checkbox

--- simple-todos.js
+++ simple-todos.js
@@ -1,28 +1,45 @@
 Tasks = new Mongo.Collection("tasks");
- 
-if (Meteor.isClient) {
-  Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({});
-    }
-  });
 
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
- 
-      // Get value from form element
-      var taskToAdd = event.target.text.value;
- 
-      // Insert a task into the collection
-      Tasks.insert({
-        text: taskToAdd,
-        createdAt: new Date() // current time
-      });
- 
-      // Clear form
-      event.target.text.value = "";
-    }
-  });
+if (Meteor.isClient) {
+    Template.body.helpers({
+        tasks: function () {
+            if (Session.get("showPromotions")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {sort: {createdAt: -1}});
+            }
+        },
+        showPromotions: function () {
+            return Session.get("showPromotions");
+
+        }
+    });
+
+    Template.body.events({
+        "submit .new-search": function (event) {
+            event.preventDefault();
+
+            var taskToAdd = event.target.localizacao.value;
+
+            Tasks.insert({
+                text: taskToAdd,
+                createdAt: new Date()
+            });
+
+            event.target.localizacao.value = "";
+        },
+        "click .toggle-checked": function () {
+            Tasks.update(this._id, {
+                $set: {checked: !this.checked}
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        },
+        "change .show-promotions input": function (event) {
+            Session.set("showPromotions", event.target.checked);
+        }
+    });
 }
